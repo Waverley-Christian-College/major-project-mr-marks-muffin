fa9e635c8e1c01477e4cb3d8282b42957b2647c3
Jeffery <li.jy@wcc.vic.edu.au>
2025-05-16 09:39:27 +1000

    Update main.py
    added .upper() to change the company name into uppercase because all of the company codes are uppercase.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ proxy_url = f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}"
 proxies = {"http": proxy_url, "https": proxy_url}
 
 # --- Yahoo Finance API config ---
-symbol = f"{Company}"
+symbol = f"{Company.upper()}"
 range_ = "1mo"
 interval = "1d"
 url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={range_}&interval={interval}"
