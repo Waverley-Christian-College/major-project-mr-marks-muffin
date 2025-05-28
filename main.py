cabb9233e05eb99183263753c609be7f884b5129
Jeffery <li.jy@wcc.vic.edu.au>
2025-05-28 00:43:26 +0000

    Updated main.py to have the API token and import the operating system

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import requests
 import json
 from datetime import datetime
 import matplotlib.pyplot as plt
+import os
+
+# Your Tiingo API token
+API_TOKEN = os.getenv("API_MUFFIN")
+print(f"This is my API Token: {API_TOKEN}")
 
 #--- User Input ---
 Company = ("Which company did you invest in? ")
